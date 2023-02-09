f5d5acf83e029a79161d590c65133b4344f53531
Davis Kiprotich <mylesdavyz@gmail.com>
2023-02-09 11:52:27 +0300

    child components

--- react-introduction/src/components/Welcome.js
+++ react-introduction/src/components/Welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Welcome extends Component{
     render(){
-        return <h1>Coding is Fun</h1>
+        return <h1>Welcome {this.props.name} ,becoming {this.props.heroName} is Fun</h1>
     }
 }
 
