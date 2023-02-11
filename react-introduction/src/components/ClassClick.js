db32334aca6cdf0604c5a4563eb29e06a1692aea
Davis Kiprotich <mylesdavyz@gmail.com>
2023-02-11 10:31:21 +0300

    Event handling

---
+++ react-introduction/src/components/ClassClick.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class ClassClick extends Component {
+  render() {
+    function clickHandler(){
+        console.log("Button has been clicked")
+    }
+    return (
+      <div>
+        <button onClick={this.clickHandler}>Click Me</button>
+      </div>
+    )
+  }
+}
+
+export default ClassClick
